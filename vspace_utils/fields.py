2f09673ffd1e5590cd2c833643dd11b07d1821e1
Mathijs de Bruin <mathijs@mathijsfietst.nl>
2013-01-26 23:25:36 +0100

    Moved in some gists.

---
+++ vspace_utils/fields.py
@@ -0,0 +1,37 @@
+import logging
+logger = logging.getLogger(__name__)
+
+# Note: we need dnspython for this to work
+# Install with `pip install dnspython`
+import dns.resolver, dns.exception
+
+from django import forms
+from django.utils.translation import ugettext as _
+
+
+class ValidatingEmailField(forms.EmailField):
+    """
+    Django EmailField which checks for MX records on the email domain.
+    Requires dnspython to be installed.
+
+    Initially published at: https://gist.github.com/876648
+    """
+
+    def clean(self, value):
+        email = super(ValidatingEmailField, self).clean(value)
+
+        domain = email.split('@')[1]
+
+        # Make sure the domain exists
+        try:
+            logger.debug('Checking domain %s', domain)
+
+            results = dns.resolver.query(domain, 'MX')
+
+        except dns.exception.DNSException, e:
+            logger.debug('Domain %s does not exist.', e)
+
+            raise forms.ValidationError(_(
+                u"The domain %s could not be found.") % domain)
+
+        return email
---
+++ vspace_utils/templatetags/next_previous.py
@@ -0,0 +1,37 @@
+from django import template
+register = template.Library()
+
+from templatetag_sugar.register import tag
+from templatetag_sugar.parser import Constant, Variable, Name
+
+from .utils import get_next_or_previous
+
+"""
+Efficient and generic get next/previous tags for the Django template language,
+using Alex Gaynor's excellent templatetag_sugar library.
+
+The library can be found at: http://pypi.python.org/pypi/django-templatetag-sugar
+
+Usage:
+
+    {% load next_previous %}
+    ...
+    {% get_next in <queryset> after <object> as <next> %}
+    {% get_previous in <queryset> before <object> as <previous> %}
+
+Initially published here: https://gist.github.com/1004216
+"""
+
+
+@tag(register, [Constant("in"), Variable(), Constant("after"), Variable(), Constant("as"), Name()])
+def get_next(context, queryset, item, asvar):
+    context[asvar] = get_next_or_previous(queryset, item, next=True)
+
+    return ""
+
+
+@tag(register, [Constant("in"), Variable(), Constant("before"), Variable(), Constant("as"), Name()])
+def get_previous(context, queryset, item, asvar):
+    context[asvar] = get_next_or_previous(queryset, item, next=False)
+
+    return ""
