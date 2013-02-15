a7d8d2ca000b9264992f1266ebeb930bbf650698
Mathijs de Bruin <mathijs@mathijsfietst.nl>
2013-02-15 13:38:24 +0100

    Added get_or_create_object.
    A get_or_create version which allows for updating of model instance 
    attributes before saving.

--- vspace_utils/utils.py
+++ vspace_utils/utils.py
@@ -1,3 +1,6 @@
+import logging
+logger = logging.getLogger(__name__)
+
 from django.db import models
 
 
@@ -20,3 +23,27 @@ def get_next_ordering(model, field_name='sort_order', increment=10):
         return latest + increment
     else:
         return increment
+
+
+def get_or_create_object(model, **kwargs):
+    """
+    Get or create feed entry with specified kwargs without saving.
+
+    This behaves like Django's own get_or_create but it doesn't save the
+    newly created object, allowing for further modification before saving
+    without triggering an extra `save()` call.
+    """
+
+    try:
+        # Updating an existing object
+        db_entry = model.objects.get(**kwargs)
+
+        logger.debug('Updating existing entry %s', db_entry)
+
+    except model.DoesNotExist:
+        # Creating a new object
+        db_entry = model(**kwargs)
+
+        logger.debug('Creating new entry %s', db_entry)
+
+    return db_entry
